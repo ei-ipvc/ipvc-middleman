884bdd38b257dc74fa6873bc418bbf9a201c5b3c
João Alves <joao.2003.couto@gmail.com>
2024-09-17 10:12:39 +0100

    fix(auth): wrong moodle year

--- src/main/kotlin/pt/joaoalves03/ipvcmiddleman/modules/moodle/Constants.kt
+++ src/main/kotlin/pt/joaoalves03/ipvcmiddleman/modules/moodle/Constants.kt
@@ -2,6 +2,6 @@ package pt.joaoalves03.ipvcmiddleman.modules.moodle
 
 object Constants {
   fun loginEndpoint(username: String, password: String): String {
-    return "https://elearning.ipvc.pt/ipvc2023/login/token.php?username=$username&password=$password&service=moodle_mobile_app"
+    return "https://elearning.ipvc.pt/ipvc2024/login/token.php?username=$username&password=$password&service=moodle_mobile_app"
   }
 }
