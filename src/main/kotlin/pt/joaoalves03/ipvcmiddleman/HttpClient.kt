a155f9bb5d72a199120ab8cb602b0aae19271fd0
João Alves <joao.2003.couto@gmail.com>
2024-09-17 16:27:05 +0100

    fix(assignments): wrong moodle year again + increased okhttp timeout

--- src/main/kotlin/pt/joaoalves03/ipvcmiddleman/HttpClient.kt
+++ src/main/kotlin/pt/joaoalves03/ipvcmiddleman/HttpClient.kt
@@ -1,7 +1,12 @@
 package pt.joaoalves03.ipvcmiddleman
 
 import okhttp3.OkHttpClient
+import java.util.concurrent.TimeUnit
 
 object HttpClient {
   val instance: OkHttpClient = OkHttpClient()
+    .newBuilder()
+    .connectTimeout(30, TimeUnit.SECONDS)
+    .readTimeout(30, TimeUnit.SECONDS)
+    .build()
 }
--- src/main/kotlin/pt/joaoalves03/ipvcmiddleman/modules/moodle/services/AuthorizationService.kt
+++ src/main/kotlin/pt/joaoalves03/ipvcmiddleman/modules/moodle/services/AuthorizationService.kt
@@ -24,6 +24,8 @@ object AuthorizationService {
       HttpClient.instance.newCall(request).execute().use { response ->
         val data = response.body!!.string()
 
+        println(data)
+
         if(data.contains("invalidlogin")) throw UnauthorizedException()
 
         val res = mapper.readValue<AuthResponse>(data)
